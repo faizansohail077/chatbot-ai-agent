657007eef9d35628fafc1a2d72e7b07d99df7339
faizan <faizansohail077@gmail.com>
2025-02-01 15:21:56 +0500

    completed landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
           {children}
         </body>
       </html>
